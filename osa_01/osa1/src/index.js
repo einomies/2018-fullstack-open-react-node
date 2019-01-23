2310f2f02a9132d3fb0578dce65d6e2180cabeef
einomies26 <einokamarainen@hotmail.com>
2019-01-23 22:53:21 +0200

    conditional rendering, simple

--- osa_01/osa1/src/index.js
+++ osa_01/osa1/src/index.js
@@ -2,53 +2,53 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      vasen: 0,
-      oikea: 0,
-      kaikki: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            vasen: 0,
+            oikea: 0,
+            kaikki: []
+        };
+    }
+
+    klikVasen = () => {
+        this.setState({
+            vasen: this.state.vasen + 1,
+            kaikki: this.state.kaikki.concat("v")
+        });
+        // console.log('vasen: this.state.vasen + 1 = ', this.state.vasen)
     };
-  }
 
-  klikVasen = () => {
-    this.setState({
-      vasen: this.state.vasen + 1,
-      kaikki: this.state.kaikki.concat("v")
-    });
-    // console.log('vasen: this.state.vasen + 1 = ', this.state.vasen)
-  };
+    klikOikea = () => {
+        this.setState({
+            oikea: this.state.oikea + 1,
+            kaikki: this.state.kaikki.concat("o")
+        });
+    };
 
-  klikOikea = () => {
-    this.setState({
-      oikea: this.state.oikea + 1,
-      kaikki: this.state.kaikki.concat("o")
-    });
-  };
+    render() {
+        const historia = () => {
+            if (this.state.kaikki.length === 0) {
+                return (
+                    <div>
+                        <em>sovellusta käytetään nappeja painelemalla</em>
+                    </div>
+                );
+            }
+            return <div>näppäilyhistoria: {this.state.kaikki.join(" ")}</div>;
+        };
 
-  render() {
-    const historia = () => {
-      if (this.state.kaikki.length === 0) {
         return (
-          <div>
-            <em>sovellusta käytetään nappeja painelemalla</em>
-          </div>
+            <div>
+                {this.state.vasen}
+                <button onClick={this.klikVasen}>vasen</button>
+                <button onClick={this.klikOikea}>oikea</button>
+                {this.state.oikea}
+                <p />
+                <div>{historia()}</div>
+            </div>
         );
-      }
-      return <div>näppäilyhistoria: {this.state.kaikki.join(" ")}</div>;
-    };
-
-    return (
-      <div>
-        {this.state.vasen}
-        <button onClick={this.klikVasen}>vasen</button>
-        <button onClick={this.klikOikea}>oikea</button>
-        {this.state.oikea}
-        <p />
-        <div>historia: {historia()}</div>
-      </div>
-    );
-  }
+    }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
