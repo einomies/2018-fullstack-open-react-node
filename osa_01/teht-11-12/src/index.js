b0ffb3df89e47ed9bc1be1203d7ec1a57c73ae1a
einomies26 <einokamarainen@hotmail.com>
2019-01-27 16:32:58 +0200

    teht 1.1 - 1.2

--- osa_01/teht-11-12/src/index.js
+++ osa_01/teht-11-12/src/index.js
@@ -1,18 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-  const { counter } = props
+const App = () => {
+  const kurssi = 'Half Stack -sovelluskehitys'
+
+  const osa1 = 'Reactin perusteet'
+  const tehtavia1 = 10
+
+  const osa2 = 'Tiedonvälitys propseilla'
+  const tehtavia2 = 7
+
+  const osa3 = 'Komponenttien tila'
+  const tehtavia3 = 14
+
+  const Otsikko = (props) => {
+    return (
+      <div>
+        <h1>{props.kurssi}</h1>
+      </div>
+    )
+  }
+
+  const Sisalto = (props) => {
+    return (
+      <div>
+        <Osa nimi={osa1} tehtLkm={tehtavia1}></Osa>
+        <Osa nimi={osa2} tehtLkm={tehtavia2}></Osa>
+        <Osa nimi={osa3} tehtLkm={tehtavia3}></Osa>
+      </div>
+    )
+  }
+
+  const Osa = (props) => {
+    return (
+      <div>
+        <p>
+          {props.nimi} {props.tehtLkm}
+        </p>
+      </div>
+    )
+  }
+
+  const Yhteensa = (props) => {
+    return (
+      <div>
+        <p>
+          yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää
+        </p>
+      </div>
+    )
+  }
+
   return (
-    <div>{counter.value}</div>
+    <div>
+      <Otsikko kurssi={kurssi}></Otsikko>
+      <Sisalto></Sisalto>
+      {/* <Yhteensa tehtLkm={tehtavia1 + tehtavia2 + tehtavia3}></Yhteensa> */}
+      <Yhteensa></Yhteensa>
+    </div>
   )
 }
 
-const counter = {
-  value: 0 + 1
-}
-
 ReactDOM.render(
-  <App counter={counter} />,
+  <App />,
   document.getElementById('root')
 )
