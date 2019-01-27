b22ea94a4718a3acb2257634f54c313fc0db9783
einomies26 <einokamarainen@hotmail.com>
2019-01-27 23:48:36 +0200

    teht 1.7 kesken

--- osa_01/teht-16-114/src/index.js
+++ osa_01/teht-16-114/src/index.js
@@ -8,32 +8,48 @@ class App extends React.Component {
     this.state = {
       hyva: 0,
       neutraali: 0,
-      huono: 0
+      huono: 0,
     }
   }
 
   klikHyva = () => {
-    this.setState({
-      hyva: this.state.hyva + 1
-    })
-    console.log(this.state)
+    this.setState({ hyva: this.state.hyva + 1 })
   }
 
-  klikNeutraali = () => {
-    this.setState({
-      neutraali: this.state.neutraali + 1
-    })
-    console.log(this.state)
-  }
+  klikNeutraali = () => this.setState({ neutraali: this.state.neutraali + 1 })
 
-  klikHuono = () => {
-    this.setState({
-      huono: this.state.huono + 1
-    })
-    console.log(this.state)
+  klikHuono = () => this.setState({ huono: this.state.huono + 1 })
+
+  summa = () => {
+    return this.state.hyva + this.state.neutraali + this.state.huono
   }
 
   render() {
+
+    const Statistiikka = () => {
+      return (
+        <div>
+          <h1>Statistiikka</h1>
+          hyvä: {this.state.hyva}
+          <p></p>
+          neutraali: {this.state.neutraali}
+          <p></p>
+          huono: {this.state.huono}
+          <p></p>
+          summa: {this.summa}
+          <p></p>
+          keskiarvo: {this.state.keskiarvo}
+          <p></p>
+          positiivisia: {this.state.positiivisia}
+        </div>
+      )
+    }
+
+    // console.log(this.state)
+    const setToValue = (newValue) => () => {
+      this.setState({ value: newValue })
+    }
+
     return (
       <div>
         <h1>Anna palautetta</h1>
@@ -41,12 +57,7 @@ class App extends React.Component {
         <button onClick={this.klikNeutraali}>neutraali</button>
         <button onClick={this.klikHuono}>huono</button>
         <p></p>
-        <h1>Statistiikka</h1>
-        hyvä: {this.state.hyva}
-        <p></p>
-        neutraali: {this.state.neutraali}
-        <p></p>
-        huono: {this.state.huono}
+        <Statistiikka />
       </div>
     );
   }
