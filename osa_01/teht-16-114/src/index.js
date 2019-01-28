a47a6c310067e434851c60fbb6073b5fd4734f48
einomies26 <einokamarainen@hotmail.com>
2019-01-28 17:51:06 +0200

    summa

--- osa_01/teht-16-114/src/index.js
+++ osa_01/teht-16-114/src/index.js
@@ -2,53 +2,55 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-
   constructor() {
-    super()
+    super();
     this.state = {
       hyva: 0,
       neutraali: 0,
       huono: 0,
-    }
+      yhteensa: 0,
+      kaikki: []
+    };
   }
 
   klikHyva = () => {
-    this.setState({ hyva: this.state.hyva + 1 })
-  }
-
-  klikNeutraali = () => this.setState({ neutraali: this.state.neutraali + 1 })
-
-  klikHuono = () => this.setState({ huono: this.state.huono + 1 })
-
-  summa = () => {
-    return this.state.hyva + this.state.neutraali + this.state.huono
-  }
+    this.setState({
+      hyva: this.state.hyva + 1,
+      yhteensa: this.state.yhteensa + 1
+    });
+  };
+  klikNeutraali = () => {
+    this.setState({
+      neutraali: this.state.neutraali + 1,
+      yhteensa: this.state.yhteensa + 1
+    });
+  };
+  klikHuono = () => {
+    this.setState({
+      huono: this.state.huono + 1,
+      yhteensa: this.state.yhteensa + 1
+    });
+  };
 
   render() {
-
     const Statistiikka = () => {
       return (
         <div>
           <h1>Statistiikka</h1>
           hyvä: {this.state.hyva}
-          <p></p>
+          <p />
           neutraali: {this.state.neutraali}
-          <p></p>
+          <p />
           huono: {this.state.huono}
-          <p></p>
-          summa: {this.summa}
-          <p></p>
+          <p />
+          yhteensä: {this.state.yhteensa}
+          <p />
           keskiarvo: {this.state.keskiarvo}
-          <p></p>
+          <p />
           positiivisia: {this.state.positiivisia}
         </div>
-      )
-    }
-
-    // console.log(this.state)
-    const setToValue = (newValue) => () => {
-      this.setState({ value: newValue })
-    }
+      );
+    };
 
     return (
       <div>
@@ -56,7 +58,7 @@ class App extends React.Component {
         <button onClick={this.klikHyva}>hyvä</button>
         <button onClick={this.klikNeutraali}>neutraali</button>
         <button onClick={this.klikHuono}>huono</button>
-        <p></p>
+        <p />
         <Statistiikka />
       </div>
     );
